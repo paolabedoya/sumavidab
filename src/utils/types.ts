38cbd638f207b9320bcd45f1ce1ff57157feba04
Nacho Martin <nachopad3@gmail.com>
2024-02-23 12:02:37 +0100

    feat: created types for each database model

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,8 @@
+export type UserRol = "admin" | "patient" | "professional"
 
 export type User = {
-    id: string
-    rol: "admin" | "patient" | "professional"
+    _id?: string
+    rol: UserRol
     job_id: number
     active: boolean
     country_id: string
@@ -9,6 +10,108 @@ export type User = {
     gender: "f" | "m"
     group_id: string
     lifestyle_id: string
+    events?: string[]
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type AppointmentType = "psycology" | "sport"
+
+export type Appointment = {
+    _id?: string
+    type: AppointmentType
+    appointment_id: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Contact = {
+    _id?: string
+    name: string
+    phone: string
+    email: string
+    message: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Country = {
+    _id?: string
+    name: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type EventVenueType = "remote" | "in_person"
+
+export type Event = {
+    _id?: string
+    start_date: Date
+    end_date: Date
+    description?: string
+    venue_type: EventVenueType
+    worker_id: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Group = {
+    _id?: string
+    name: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type JobName = "nutricionist" | "psychologist" | "trainer"
+
+export type Job = {
+    _id?: string
+    name: JobName
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Lifestyle = {
+    _id?: string
+    name: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type MedicalHistory = {
+    _id?: string
+    patient_id: string
+    appointment_id: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type MenuRecommendation = {
+    _id?: string
+    breakfast: string
+    brunch: string
+    lunch: string
+    afternoon_snack: string
+    dinner: string
+    image_url: string
+    professional_id: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Recipe = {
+    _id?: string
+    name: string
+    ingredients: string[]
+    steps: string[]
+    image_url: string
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export type Region = {
+    _id?: string
+    name: string
     createdAt?: Date
     updatedAt?: Date
 }
